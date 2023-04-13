0ec1d82d1ca6711ea8838c8612a8db00a300bb00
Jared <jared.trejo@axity.com>
2023-04-12 19:30:56 -0600

    Deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Piano';
+
+  playAudio(n: number): void {
+    const audio = new Audio();
+    audio.src = `../assets/audios/note${n}.wav`;
+    audio.load();
+    audio.play();
+  }
 }
